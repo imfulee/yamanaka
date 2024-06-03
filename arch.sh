3694e26791a30322dc11fe741b228e812402d9cd
imfulee <freems5192@gmail.com>
2024-06-03 13:37:15 +0800

    feat: zsh files

---
+++ arch.sh
@@ -0,0 +1,35 @@
+ratemirrors() {
+	rate-mirrors arch | sudo tee /etc/pacman.d/mirrorlist
+	rate-mirrors endeavouros | sudo tee /etc/pacman.d/endeavouros-mirrorlist
+}
+
+updatesoftware() {
+	if [ -f "/etc/arch-release" ]; then
+		sudo pacman -Syu
+		yay -Syu
+	fi
+
+	if [ -f "/usr/bin/flatpak" ]; then
+		flatpak update -y
+	fi
+}
+
+updatefirmware() {
+	fwupdmgr refresh --force
+	fwupdmgr update
+}
+
+updateall() {
+	updatesoftware
+	updatefirmware
+}
+
+# pacman browse remote packages
+pkgsremote() {
+	pacman -Slq | fzf --preview 'pacman -Si {}' --layout=reverse
+}
+
+# pacman browse installed packages
+pkgsinstalled() {
+	pacman -Qq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'
+}
