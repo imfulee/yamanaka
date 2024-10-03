96ad3e35bea6652ad07ee8ecc49bc736ac4bd747
imfulee <freems5192@gmail.com>
2024-10-03 16:09:23 +0800

    format: arch.sh

--- arch.sh
+++ arch.sh
@@ -1,36 +1,36 @@
 ratemirrors() {
-	rate-mirrors arch | sudo tee /etc/pacman.d/mirrorlist
-	rate-mirrors endeavouros | sudo tee /etc/pacman.d/endeavouros-mirrorlist
+  rate-mirrors arch | sudo tee /etc/pacman.d/mirrorlist
+  rate-mirrors endeavouros | sudo tee /etc/pacman.d/endeavouros-mirrorlist
 }
 
 updatesoftware() {
-	if [ -f "/etc/arch-release" ]; then
-		sudo pacman -Syu
-		yay -Syu
-	fi
+  if [ -f "/etc/arch-release" ]; then
+    sudo pacman -Syu
+    yay -Syu
+  fi
 
-	if [ -f "/usr/bin/flatpak" ]; then
-		flatpak update -y
-		flatpak remove --unused
-	fi
+  if [ -f "/usr/bin/flatpak" ]; then
+    flatpak update -y
+    flatpak remove --unused
+  fi
 }
 
 updatefirmware() {
-	fwupdmgr refresh --force
-	fwupdmgr update
+  fwupdmgr refresh --force
+  fwupdmgr update
 }
 
 updateall() {
-	updatesoftware
-	updatefirmware
+  updatesoftware
+  updatefirmware
 }
 
 # pacman browse remote packages
 pkgsremote() {
-	pacman -Slq | fzf --preview 'pacman -Si {}' --layout=reverse
+  pacman -Slq | fzf --preview 'pacman -Si {}' --layout=reverse
 }
 
 # pacman browse installed packages
 pkgsinstalled() {
-	pacman -Qq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'
+  pacman -Qq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'
 }
